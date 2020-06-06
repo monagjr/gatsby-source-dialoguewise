958ff80a4e39d527e4de195f03dc219fb55d55ab
M. Jr <monagjr@gmail.com>
2020-06-06 01:04:46 +0000

    Format Code

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,92 +2,93 @@ const fetch = require("node-fetch");
 const moment = require("moment");
 const crypto = require("crypto");
 
-const DIALOGUE_NODE_TYPE = `Dialogue`
+const DIALOGUE_NODE_TYPE = `Dialogue`;
 
 exports.sourceNodes = async (
   { actions, createNodeId, createContentDigest },
   pluginOptions
 ) => {
-  const { createNode } = actions
-  
+  const { createNode } = actions;
+
   const apiBaseUrl = "https://api.dialoguewise.com/api/";
   const requests = {
     ...{
-      apiKey: '',
-      emailHash: '',
-      dialogues: []
+      apiKey: "",
+      emailHash: "",
+      dialogues: [],
     },
     ...pluginOptions,
   };
 
-  var nodes = []
+  var nodes = [];
 
-  for (const dialogue of requests.dialogues ) {
+  for (const dialogue of requests.dialogues) {
     // make sure we have the mandatory dialougeName
-    if (!dialogue.hasOwnProperty('name') || dialogue.name.trim() == ''){
-      throw "Name field is mandatory"
+    if (!dialogue.hasOwnProperty("name") || dialogue.name.trim() == "") {
+      throw "Name field is mandatory";
     }
 
-      const request = {
-        apiKey: requests.apiKey,
-        emailHash: requests.emailHash,
-        dialogueName: dialogue.name,
-        isPilot: dialogue.isPilot,
-        variableList: dialogue.variableList
+    const request = {
+      apiKey: requests.apiKey,
+      emailHash: requests.emailHash,
+      dialogueName: dialogue.name,
+      isPilot: dialogue.isPilot,
+      variableList: dialogue.variableList,
+    };
+
+    const currentUtc = moment.utc().format("DD/MM/YYYY hh:mm:ss a");
+    const isPilotFlag = request.isPilot ? "&isPilotVersion=true" : "";
+    const apiUrl =
+      apiBaseUrl +
+      "dialogue/getdialogue?dialogueName=" +
+      request.dialogueName +
+      isPilotFlag;
+
+    const message = "/api/dialogue/getdialogue:" + currentUtc;
+    const key = request.apiKey;
+    const hashMessage = crypto
+      .createHmac("sha256", key)
+      .update(message)
+      .digest("base64");
+
+    const authentication = request.emailHash + ":" + hashMessage;
+    const headers = {
+      "Content-Type": "application/json",
+      Timestamp: currentUtc,
+      Authentication: authentication,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "Content-Type, Timestamp, Authentication",
+    };
+
+    var nodeDialogueData = {
+      name: request.dialogueName,
+      content: "",
+      error: "",
+    };
+    try {
+      const response = await fetch(apiUrl, {
+        method: "post",
+        headers: headers,
+        body: JSON.stringify(request.variableList),
+      });
+
+      if (response.status !== 200) {
+        nodeDialogueData.error = response.statusText;
+      } else {
+        const respJson = await response.json();
+        nodeDialogueData.content = respJson.dialogue
+          ? JSON.stringify(respJson.dialogue)
+          : "";
       }
-
-      const currentUtc = moment.utc().format("DD/MM/YYYY hh:mm:ss a");
-      const isPilotFlag = request.isPilot ? "&isPilotVersion=true" : "";
-      const apiUrl =
-        apiBaseUrl +
-        "dialogue/getdialogue?dialogueName=" +
-        request.dialogueName +
-        isPilotFlag;
-      
-      const message = "/api/dialogue/getdialogue:" + currentUtc;
-      const key = request.apiKey;
-      const hashMessage = crypto
-        .createHmac("sha256", key)
-        .update(message)
-        .digest("base64");
-
-      const authentication = request.emailHash + ":" + hashMessage;
-      const headers = {
-        "Content-Type": "application/json",
-        Timestamp: currentUtc,
-        Authentication: authentication,
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "Content-Type, Timestamp, Authentication",
-      };
-
-      var nodeDialogueData = {
-        name: request.dialogueName,
-        content: '',
-        error: ''
-      }
-      try {
-        const response = await fetch(apiUrl, {
-          method: "post",
-          headers: headers,
-          body: JSON.stringify(request.variableList),
-        })
-
-        if (response.status !== 200) {
-            nodeDialogueData.error = response.statusText
-        } else {
-            const respJson = await response.json();
-            nodeDialogueData.content = respJson.dialogue ? JSON.stringify(respJson.dialogue) : ''
-        }
-      } catch (err) { 
-        nodeDialogueData.error = err.message
-      }
-      nodes.push(nodeDialogueData)
-    
+    } catch (err) {
+      nodeDialogueData.error = err.message;
+    }
+    nodes.push(nodeDialogueData);
   }
 
   // loop through data and create Gatsby nodes
-  nodes.forEach(node =>
+  nodes.forEach((node) =>
     createNode({
       ...node,
       id: createNodeId(`${DIALOGUE_NODE_TYPE}-${node.name}`),
@@ -99,9 +100,9 @@ exports.sourceNodes = async (
         contentDigest: createContentDigest(node),
       },
     })
-  )
-  
-  return
-}
+  );
+
+  return;
+};
 
 exports.onPreInit = () => console.log("Loaded dialoguewise-source-plugin");
